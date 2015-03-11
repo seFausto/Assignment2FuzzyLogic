57e998bc8744113f58a8c53c1eae05bbd1b25c95
Fausto Ruvalcaba <se.fausto@gmail.com>
2015-03-10 18:58:11 -0700

    Changes

--- src/FuzzyLogic.java
+++ src/FuzzyLogic.java
@@ -7,37 +7,20 @@ public class FuzzyLogic {
 
 		Logger.TurnLogginOn = true;
 
-		StockGenerator s = new StockGenerator();
-		MadGenerator m = new MadGenerator();
+		StockGenerator stockGenerator = new StockGenerator();
+		MadGenerator madGenerator = new MadGenerator();
 
-		Fuzzy sf = new Fuzzy();
-		sf.RangeMax = 20;
-		sf.RangeMin = 0;
+		Fuzzy sf = SetStocksFuzzy();
 
-		sf.Inputs = new ArrayList<FuzzyInput>();
-
-		sf.Inputs.add(new FuzzyInput("VeryLow", 0, 4));
-		sf.Inputs.add(new FuzzyInput("Low", 4, 10));
-		sf.Inputs.add(new FuzzyInput("Medium", 9, 14));
-		sf.Inputs.add(new FuzzyInput("High", 13, 17));
-		sf.Inputs.add(new FuzzyInput("VeryHigh", 16, Double.MAX_VALUE));
-
-		Fuzzy madFuzzy = new Fuzzy();
-		madFuzzy.RangeMax = 1;
-		madFuzzy.RangeMin = -1;
-		madFuzzy.Inputs = new ArrayList<FuzzyInput>();
-		madFuzzy.Inputs.add(new FuzzyInput("Negative",
-				Double.NEGATIVE_INFINITY, -1));
-		madFuzzy.Inputs.add(new FuzzyInput("Zero", 0, 0));
-		madFuzzy.Inputs.add(new FuzzyInput("Positive", 1,
-				Double.POSITIVE_INFINITY));
+		Fuzzy madFuzzy = SetMadFuzzy();
 
 		Broker broker = new Broker(1200);
 		double lastStockPrice = 0;
+		
 		for (int day = 1; day <= 150; day++) {
 
-			double stockPrice = s.GetStockPrice(day);
-			double mad = m.GetMad(day);
+			double stockPrice = stockGenerator.GetStockPrice(day);
+			double mad = madGenerator.GetMad(day);
 
 			List<String> values = sf.GetFuzzyValue(stockPrice);
 			List<String> madValues = madFuzzy.GetFuzzyValue(mad);
@@ -70,5 +53,34 @@ public class FuzzyLogic {
 		System.out.println("TOTAL MONEY: "
 				+ (broker.Money + (broker.NumberOfStocks * lastStockPrice)));
 
+	}
+
+	private static Fuzzy SetMadFuzzy() {
+		Fuzzy madFuzzy = new Fuzzy();
+		madFuzzy.RangeMax = 1;
+		madFuzzy.RangeMin = -1;
+		madFuzzy.Inputs = new ArrayList<FuzzyInput>();
+		madFuzzy.Inputs.add(new FuzzyInput("Negative",
+				Double.NEGATIVE_INFINITY, -1));
+		madFuzzy.Inputs.add(new FuzzyInput("Zero", 0, 0));
+		madFuzzy.Inputs.add(new FuzzyInput("Positive", 1,
+				Double.POSITIVE_INFINITY));
+		return madFuzzy;
+	}
+
+	private static Fuzzy SetStocksFuzzy() {
+		Fuzzy sf = new Fuzzy();
+		sf.RangeMax = 20;
+		sf.RangeMin = 0;
+
+		//
+		sf.Inputs = new ArrayList<FuzzyInput>();
+
+		sf.Inputs.add(new FuzzyInput("VeryLow", 0, 4));
+		sf.Inputs.add(new FuzzyInput("Low", 4, 10));
+		sf.Inputs.add(new FuzzyInput("Medium", 9, 14));
+		sf.Inputs.add(new FuzzyInput("High", 13, 17));
+		sf.Inputs.add(new FuzzyInput("VeryHigh", 16, Double.MAX_VALUE));
+		return sf;
 	}
 }
